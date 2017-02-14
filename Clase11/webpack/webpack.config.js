160b663b0c372cd29b94a7bac55f30a09df8db83
Sergio <sergiohidalgocaceres@gmail.com>
2017-02-13 21:44:32 -0500

    Clase11

---
+++ Clase11/webpack/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+ var webpack = require('webpack');
+
+/*{
+    entry: {
+        a: "./a",
+        b: "./b",
+        c: ["./c", "./d"]
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].entry.js"
+    }
+}*/
+
+
+
+
+ module.exports = {
+     entry: './js/main.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'main.bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
